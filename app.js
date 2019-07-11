d5a420327f34a2498ed44cfceffd252055081bfc
Sangoon_Is_Noob <40298015+cotwo0139@users.noreply.github.com>
2019-07-11 15:41:12 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,9 @@ const Discord = require('discord.js');
 
 class Bot {
     constructer (options) {
+        this.client = new Discord.Client({ disableEveryone: true });
+        this.options = options;
     }
+
+    
 }
